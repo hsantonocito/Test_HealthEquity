7f79f96486b8907eae05476e40fa5ea7daf855a7
Hernan Santonocito <hsantonocito@gmail.com>
2022-11-17 19:13:15 -0300

    Definitive upload
    Definitive upload

--- Exercise1_WebAppCarsCrud/WebAppCarsCrud/Controllers/CarsApiController.cs
+++ Exercise1_WebAppCarsCrud/WebAppCarsCrud/Controllers/CarsApiController.cs
@@ -20,10 +20,47 @@ namespace WebAppCarsCrud.Controllers
             return Ok(list);
         }
 
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetCarsById(int id)
+        {
+            var c = db.Cars.Where(model => model.Id == id).FirstOrDefault();
+            return Ok(c);
+        }
+
         [System.Web.Http.HttpPost]
         public IHttpActionResult CarInsert(Cars c)
         {
             db.Cars.Add(c);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult CarUpdate(Cars c)
+        {
+            var car = db.Cars.Where(model => model.Id == c.Id).FirstOrDefault();
+            if (car != null) {
+                car.Id = c.Id;
+                car.Make = c.Make;
+                car.Model = c.Model;
+                car.Year = c.Year;
+                car.Doors = c.Doors;
+                car.Color = c.Color;
+                car.Price = c.Price;
+                db.SaveChanges();
+            } else
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [System.Web.Http.HttpDelete]
+        public IHttpActionResult CarDelete(int id)
+        {
+            var car = db.Cars.Where(model => model.Id == id).FirstOrDefault();
+            db.Entry(car).State = System.Data.Entity.EntityState.Deleted;
             db.SaveChanges();
             return Ok();
         }
--- Exercise1_WebAppCarsCrud/WebAppCarsCrud/Controllers/CarsMvcController.cs
+++ Exercise1_WebAppCarsCrud/WebAppCarsCrud/Controllers/CarsMvcController.cs
@@ -24,21 +24,22 @@ namespace WebAppCarsCrud.Controllers
                 var display = test.Content.ReadAsAsync<List<Cars>>();
                 display.Wait();
                 carsList = display.Result;
-
             }
 
             return View(carsList);
         }
 
-        public ActionResult Create() {
+        public ActionResult Create()
+        {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(Cars c) {
+        public ActionResult Create(Cars c)
+        {
 
             client.BaseAddress = new Uri("https://localhost:44399/api/CarsApi");
-            var response = client.PostAsJsonAsync<Cars>("CarsApi",c);
+            var response = client.PostAsJsonAsync<Cars>("CarsApi", c);
             response.Wait();
 
             var test = response.Result;
@@ -46,10 +47,97 @@ namespace WebAppCarsCrud.Controllers
             if (test.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
-
             }
 
             return View("Create");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Cars car = null;
+            client.BaseAddress = new Uri("https://localhost:44399/api/CarsApi");
+            var response = client.GetAsync("CarsApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Cars>();
+                display.Wait();
+                car = display.Result;
+            }
+
+            return View(car);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Cars c = null;
+            client.BaseAddress = new Uri("https://localhost:44399/api/CarsApi");
+            var response = client.GetAsync("CarsApi?id="+id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Cars>();
+                display.Wait();
+                c = display.Result;
+            }
+
+            return View(c);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Cars c)
+        {
+            client.BaseAddress = new Uri("https://localhost:44399/api/CarsApi");
+            var response = client.PutAsJsonAsync<Cars>("CarsApi", c);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            return View("Edit");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Cars c = null;
+            client.BaseAddress = new Uri("https://localhost:44399/api/CarsApi");
+            var response = client.GetAsync("CarsApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Cars>();
+                display.Wait();
+                c = display.Result;
+            }
+
+            return View(c);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44399/api/CarsApi");
+            var response = client.DeleteAsync("CarsApi/" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Delete");
         }
     }
 }
